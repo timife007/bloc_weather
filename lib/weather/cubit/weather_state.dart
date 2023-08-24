afeedb4b240351d7c4b8b215e28e01e2a72aa656
Timothy Ademola <timife007@gmail.com>
2023-08-24 21:32:06 +0100

    weather state, event, bloc impl

---
+++ lib/weather/cubit/weather_state.dart
@@ -0,0 +1,47 @@
+part of 'weather_cubit.dart';
+
+enum WeatherStatus { initial, loading, success, failure }
+
+///name convention for extension function,not doing anything really.
+extension WeatherStatusX on WeatherStatus {
+  bool get isInitial => this == WeatherStatus.initial;
+  bool get isLoading => this == WeatherStatus.loading;
+  bool get isSuccess => this == WeatherStatus.success;
+  bool get isFailure => this == WeatherStatus.failure;
+}
+
+
+
+@JsonSerializable()
+final class WeatherState extends Equatable {
+  ///How classes are initialized in dart.
+  WeatherState({
+    this.status = WeatherStatus.initial,
+    this.temperatureUnits = TemperatureUnits.celsius,
+    Weather? weather,
+  }) : weather = weather ?? Weather.empty;
+
+  factory WeatherState.fromJson(Map<String, dynamic> json) =>
+      _$WeatherStateFromJson(json);
+
+  final WeatherStatus status;
+  final Weather weather;
+  final TemperatureUnits temperatureUnits;
+
+  WeatherState copyWith({
+    WeatherStatus? status,
+    TemperatureUnits? temperatureUnits,
+    Weather? weather,
+  }) {
+    return WeatherState(
+      status: status ?? this.status,
+      temperatureUnits: temperatureUnits ?? this.temperatureUnits,
+      weather: weather ?? this.weather,
+    );
+  }
+
+  Map<String, dynamic> toJson() => _$WeatherStateToJson(this);
+
+  @override
+  List<Object?> get props => [status, temperatureUnits, weather];
+}
